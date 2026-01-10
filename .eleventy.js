e8c3dcc7e0efda18146736689acbd518078937c8
Hrishikesh Barman <oss@geekodour.org>
2026-01-10 16:09:02 +0530

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("NoticeForWebsiteAPSC.pdf");
 
   return {
     dir: {
